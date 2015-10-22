7d52d9946a4209166450f4573d4cc9806c6c718b
Willy Yang <tinysqueak@gmail.com>
2015-10-22 09:43:11 -0700

    Exercises 15.2 & 15.3

---
+++ Think-AP-Java/Chapter-15/ComplexDemo.java
@@ -0,0 +1,51 @@
+
+/**
+ * Exercises 15.1 & 15.2
+ * @author William Yang
+ *
+ */
+public class ComplexDemo {
+	
+	public static void main(String[] args) {
+
+		Complex c = new Complex(3, 4);
+		Complex d = new Complex(3, 4);
+		Complex e = new Complex(5, 12);
+		
+		System.out.println(c.abs());
+		System.out.println(e.abs());
+		
+		System.out.println();
+
+		System.out.println(Complex.equals(c, d));
+		System.out.println(Complex.equals(c, e));
+		
+	}
+
+}
+
+class Complex {
+
+	private int real;
+	private int imag;
+
+	public Complex(int real, int imag) {
+
+		this.real = real;
+		this.imag = imag;
+
+	}
+
+	public double abs() {
+
+		return Math.sqrt(this.real * this.real + this.imag * this.imag);
+
+	}
+
+	public static boolean equals(Complex a, Complex b) {
+
+		return(a.real == b.real && a.imag == b.imag);
+
+	}
+
+}
