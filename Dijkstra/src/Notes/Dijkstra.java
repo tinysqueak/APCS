80f2cae8530c44a2050878462a8d276ec68cbfdc
Willy Yang <tinysqueak@gmail.com>
2016-01-14 09:18:35 -0800

    Reorganization V2

---
+++ Dijkstra/src/Notes/Dijkstra.java
@@ -0,0 +1,33 @@
+package Notes;
+
+import java.util.*;
+
+public class Dijkstra {
+
+	//the previous node from a given node
+	public HashMap<String, String> path;
+
+	String startNode;
+
+	//whether is minimum distance or not, selected set
+	private ArrayList<Boolean> k;
+
+	//node names
+	private ArrayList<String> nodes;
+
+	//previous node for a given node
+	private ArrayList<String> prev;
+
+	//cumulative distance from start node
+	private ArrayList<String> d;
+
+	public Dijkstra(HashMap<String, HashMap<String, Integer>> nodeDistances, String start) {
+		
+		//supposed to use priority queue?
+		
+		
+	}
+
+
+
+}
