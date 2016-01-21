9e218e38b813a1414895ecc35d47a1a72b2eb386
Willy Yang <tinysqueak@gmail.com>
2016-01-21 09:48:48 -0800

    More Dijkstra notes and working GenericMax

--- GenericMax/GenericMax.java
+++ GenericMax/GenericMax.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+
+import java.util.*;
 
 /**
  * 
@@ -6,30 +7,36 @@ import java.util.ArrayList;
  *
  * @param <T>
  */
-public class GenericMax<T extends Comparable<T>> extends ArrayList<T>{
+public class GenericMax<T extends Comparable<T>> extends PriorityQueue<T>{
 
+	public GenericMax(Collection<T> initialCollection) {
+		
+		addAll(initialCollection);
+		
+	}
+	
 	public GenericMax() {
 		
-		add(null);
+		//add(null);
 		
 	}
 	
 	//public GenericMax
-
+	
 	public T genericMax() {
 		
-		int finalIndex = size();
-
-		if(get(1).compareTo(get(2)) < 0 || get(1).compareTo(get(3)) < 0) {
-			
-			
-			
-		}
-		
-		return null;
+		return Collections.max(this);
 
 	}
 	
+	//static version
+	public static <T extends Comparable<T>> T staticGenericMax(Collection<T> initialCollection) {
+		
+		return Collections.max(initialCollection);
+		
+	}
+	
+	/*
 	@Override
 	public boolean add(T element) {
 		
@@ -53,11 +60,27 @@ public class GenericMax<T extends Comparable<T>> extends ArrayList<T>{
 		set(index1, get(index2));
 		set(index2, temp);
 		
-	}
+	}*/
 	
 	public static void main(String[] args) {
 
+		ArrayList<Integer> test = new ArrayList<Integer>();
+		test.add(1);
+		test.add(2);
+		test.add(3);
+		
+		ArrayList<GenericMax> test1 = new ArrayList<GenericMax>();
+		
+		
+		System.out.println("New Max: " + newMax(test));
+		
+		GenericMax<Integer> asdf2 = new GenericMax<Integer>(test);
+		System.out.println(Collections.max(asdf2));
+		
 		GenericMax<Integer> asdf = new GenericMax<Integer>();
+		asdf.add(1);
+		asdf.add(2);
+		System.out.println(Collections.max(asdf));
 		
 	}
 
