7d17aa0aeef06c738104173dd0c7b08ecaf8b95a
Willy Yang <tinysqueak@gmail.com>
2016-01-21 18:02:17 -0800

    Fixed up tests

--- GenericMax/GenericMax.java
+++ GenericMax/GenericMax.java
@@ -4,8 +4,6 @@ import java.util.*;
 /**
  * 
  * @author William Yang
- *
- * @param <T>
  */
 public class GenericMax<T extends Comparable<T>> extends PriorityQueue<T>{
 
@@ -16,12 +14,8 @@ public class GenericMax<T extends Comparable<T>> extends PriorityQueue<T>{
 	}
 	
 	public GenericMax() {
-		
-		//add(null);
-		
+				
 	}
-	
-	//public GenericMax
 	
 	public T genericMax() {
 		
@@ -36,32 +30,6 @@ public class GenericMax<T extends Comparable<T>> extends PriorityQueue<T>{
 		
 	}
 	
-	/*
-	@Override
-	public boolean add(T element) {
-		
-		//add(element);
-		
-		for(int index = size() - 1; index > 1; index /= 2) {
-			
-			
-			
-		}
-		
-		super.add(element);
-		
-		return true;
-		
-	}
-	
-	private void swap(int index1, int index2) {
-		
-		T temp = get(index1);
-		set(index1, get(index2));
-		set(index2, temp);
-		
-	}*/
-	
 	public static void main(String[] args) {
 
 		ArrayList<Integer> test = new ArrayList<Integer>();
@@ -69,17 +37,16 @@ public class GenericMax<T extends Comparable<T>> extends PriorityQueue<T>{
 		test.add(2);
 		test.add(3);
 				
+		System.out.println("Max Integer: " + staticGenericMax(test));
 		
-		System.out.println("New Max: " + staticGenericMax(test));
+		GenericMax<String> test2 = new GenericMax<String>();
+		test2.add("Generic");
+		test2.add("Max");
+		test2.add("of");
+		test2.add("a collection of");
+		test2.add("Strings");
 		
-		GenericMax<Integer> asdf2 = new GenericMax<Integer>(test);
-		System.out.println(Collections.max(asdf2));
-		
-		GenericMax<Integer> asdf = new GenericMax<Integer>();
-		asdf.add(1);
-		asdf.add(2);
-		System.out.println(Collections.max(asdf));
-		
+		System.out.println(test2.genericMax());
 	}
 
 }
