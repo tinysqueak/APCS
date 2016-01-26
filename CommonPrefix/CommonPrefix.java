4830ca316acf355407f8d50196ccc4cd33da2a6e
Willy Yang <tinysqueak@gmail.com>
2016-01-26 08:55:10 -0800

    Fixed and finished

--- CommonPrefix/CommonPrefix.java
+++ CommonPrefix/CommonPrefix.java
@@ -1,31 +1,50 @@
 
+/**
+ * 
+ * @author William Yang
+ *
+ */
 public class CommonPrefix {
 	
+	/**
+	 * Runs some test cases of the method.
+	 * @param args Unused parameter
+	 */
 	public static void main(String[] args) {
 			
-		System.out.println(common("asdf", "asdfghjkl"));
+		System.out.println(common("Hello", "Hehaw"));
+		System.out.println(common("hello", "Hehaw"));
+		System.out.println(common("asdf", "asdfghjk"));
 		
 	}
 	
+	/**
+	 * Determines the common prefix of two Strings.
+	 * @param string1 The first String to find the common prefix of.
+	 * @param string2 The second String to find the common prefix of.
+	 * @return The common prefix of the two Strings.
+	 */
 	public static String common(String string1, String string2) {
 		
 		String commonPrefix = "";
 		int shorterLength = (string1.length() < string2.length()) ? string1.length() : string2.length();
 		
-		/*String shorterString = (string1.length() < string2.length()) ? string1 : string2;
-		String longerString = (string1.length() < string2.length()) ? string2 : string1;*/
-		
-		int i = 0;
-		while(string1.charAt(i) == string2.charAt(i) && i < shorterLength) {
+		for(int i = 0; i < shorterLength; i++) {
 			
-			commonPrefix += string1.charAt(i);
-			i++;
+			if(string1.charAt(i) == string2.charAt(i)) {
+				
+				commonPrefix += string1.charAt(i);
+				
+			} else {
+				
+				break;
+				
+			}
 			
 		}
 		
 		return commonPrefix;
 		
 	}
-	
 	
 }
