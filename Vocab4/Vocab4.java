d2261172dafe86fea97d1d96d7b92ff919091e9c
Willy Yang <tinysqueak@gmail.com>
2016-01-28 09:25:00 -0800

    Finished adjacency list

--- Vocab4/Vocab4.java
+++ Vocab4/Vocab4.java
@@ -20,7 +20,25 @@ public class Vocab4 {
 		System.out.println(stringList);
 		
 		//adjacency list representation of a graph
+		ArrayList<String> aNodes = new ArrayList<String>();
+		ArrayList<String> bNodes = new ArrayList<String>();
+		ArrayList<String> cNodes = new ArrayList<String>();
 		
+		aNodes.add("B");
+		aNodes.add("C");
+		
+		bNodes.add("A");
+		bNodes.add("C");
+		
+		cNodes.add("A");
+		cNodes.add("B");
+		
+		HashMap<String, ArrayList<String>> adjacencyList = new HashMap<String, ArrayList<String>>();
+		adjacencyList.put("A", aNodes);
+		adjacencyList.put("B", bNodes);
+		adjacencyList.put("C", cNodes);
+		
+		System.out.println("This is an adjacency list representation of a graph: " + adjacencyList);
 		
 	}
 
