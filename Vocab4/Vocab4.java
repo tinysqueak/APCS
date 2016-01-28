6b90fcabd686d5ce30bad70f404f9207be083567
Willy Yang <tinysqueak@gmail.com>
2016-01-27 23:20:27 -0800

    Use adjacency list for graph

---
+++ Vocab4/Vocab4.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class Vocab4 {
+
+	public static void main(String[] args) {
+
+		String string = "This is a string";
+		
+		System.out.println(string);
+		
+		ArrayList<String> stringList = new ArrayList<String>();
+		stringList.add("This");
+		stringList.add("is");
+		stringList.add("a");
+		stringList.add("Collection");
+		stringList.add("of");
+		stringList.add("Strings");
+		
+		System.out.println(stringList);
+		
+		//adjacency list representation of a graph
+		
+		
+	}
+
+}
